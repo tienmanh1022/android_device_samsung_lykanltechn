054d4543b233592c502bb4b1ec8eb4fe624e9cbe
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-08-05 14:50:46 +0200

    lykanltechn: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-W2019 Script version: 3.0.0

---
+++ omni_lykanltechn.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from lykanltechn device
+$(call inherit-product, device/samsung/lykanltechn/device.mk)
+
+PRODUCT_DEVICE := lykanltechn
+PRODUCT_NAME := omni_lykanltechn
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-W2019
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lykanltezc-user 10 QP1A.190711.020 W2019ZCU2CUK1 release-keys"
+
+BUILD_FINGERPRINT := samsung/lykanltezc/lykanltechn:10/QP1A.190711.020/W2019ZCU2CUK1:user/release-keys
